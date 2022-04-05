dd4d27cf1a9960ef64d4d78f3da063321838ddb4
dhruv50ae <linkdp2012@gmail.com>
2022-04-05 16:30:36 +0100

    Created leap year calc

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-height = float(input("Enter your height in m: "))
-weight = float(input("Enter your weight in kg: "))
-bmi = round(weight/height ** 2)
+year = int(input("Which year do you want to check? "))
 
-if bmi < 18.5:
-    print(f"Your BMI is {bmi}, you are underweight.")
-elif bmi < 25:
-    print(f"Your BMI is {bmi}, you have normal weight")
-elif bmi < 30:
-    print(f"Your BMI is {bmi}, you are overweight.")
-elif bmi < 35:
-    print(f"Your BMI is {bmi}, you are obease.")
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not a leap year.")
+    else:
+        print("Leap year.")
 else:
-    print(f"Your BMI is {bmi}, you are clinically obease.")
+    print("Not a leap year.")
