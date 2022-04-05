83224166fd145b35b4b0a8f4a8c7cbd8cbe9f1a0
dhruv50ae <linkdp2012@gmail.com>
2022-04-05 18:28:28 +0100

    Endgame commit
    In case you are wondering what the reference is. It is League of Legends chat in a nutshell. The only way to beat toxicity is to pay no attention to it so every comment holds no gravity

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-year = int(input("Which year do you want to check? "))
-
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            print("Leap year.")
+print("Welcome to Tresure Island\nYour mission is to find the tresure.")
+choice1 = input(
+    "You\'re at a crossroad, where do you want to go? Type 'left' or 'right'. ").lower()
+if choice1 == 'left':
+    choice2 = input(
+        "You\'ve come to a lake. There is an island in the middle of the lake. Do you choose to 'wait' or 'swim' across? ")
+    if choice2 == 'wait':
+        choice3 = input(
+            "You arrive at the island thanks to Gangplank. There is a house with 3 doors. One red, one yellow, one blue. Which colour do you pick? ").lower()
+        if choice3 == 'red':
+            print("You overextended a Brand mid, gg ez Brand fed, Game Over. ")
+        elif choice3 == 'yellow':
+            print("You managed to get to the Tresure, being full build at 20 minutes. Now you are the diff across all lanes 😈.")
+        elif choice3 == 'blue':
+            print("You face a Garen jungle and get 13/0'd, gg ez jungle diff. Game Over.")
         else:
-            print("Not a leap year.")
+            print("You didn't pick a door and end up getting 1v5'd where everyone used their ult. gg ez team diff. Game Over.")
     else:
-        print("Leap year.")
+        print("You tried to swim but Tahm Kench one shot you with his ult, gg ez top diff, Game Over.")
 else:
-    print("Not a leap year.")
+    print("You're facing a level 18 Yasuo, gg ez ADC diff, Game Over.")
