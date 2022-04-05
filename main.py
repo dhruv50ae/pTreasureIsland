9e2065e25428c73c33cbc77fb944b75fe7079f8f
dhruv50ae <linkdp2012@gmail.com>
2022-04-05 14:41:07 +0100

    Added elifs

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-number = int(input("Which number do you want to check? "))
-if number % 2 == 0:
-    print("This is an even number")
+print("Welcome to the rollercoaster")
+
+height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+    age = int(input('What is your age? '))
+    if age < 12:
+        print("That will be $5 please")
+    elif age <= 18:
+        print("That will be $7 please")
+    else:
+        print("That will be $12 please")
 else:
-    print('This is an odd number')
+    print("Sorry, you have to grow taller before you can ride.")
